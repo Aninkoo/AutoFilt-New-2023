e8f234ce299c79bb939400a65ffa3c952108b9f6
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-11-18 18:50:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -139,7 +139,8 @@ async def next_page(bot, query):
     else:
         btn = []
         for file in files:
-            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
+            c_data = f"{pre}#{file.file_id}"
+            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start={c_data}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
     try:
         if settings['auto_delete']:
             btn.insert(0, 
@@ -382,7 +383,8 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
     else:
         btn = []
         for file in files:
-            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
+            c_data = f"{pre}#{file.file_id}"
+            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start={c_data}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
     try:
         if settings['auto_delete']:
             btn.insert(0, 
@@ -484,7 +486,8 @@ async def lang_next_page(bot, query):
     else:
         btn = []
         for file in files:
-            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
+            c_data = f"{pre}#{file.file_id}"
+            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start={c_data}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
 
     try:
         if settings['auto_delete']:
@@ -1624,8 +1627,8 @@ async def auto_filter(client, msg, spoll=False):
     else:
         btn = []
         for file in files:
-            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
-
+            c_data = f"{pre}#{file.file_id}"
+            files_link += f"""<b>\n\n‼️ <a href=https://t.me/{temp.U_NAME}?start={c_data}>[{get_size(file.file_size)}] {file.file_name}</a></b>"""
     try:
         if settings['auto_delete']:
             btn.insert(0, 
