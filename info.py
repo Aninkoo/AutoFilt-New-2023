eea2cacf3f33cc66219d58b89068988b3467f587
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-12-21 00:24:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,6 +49,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID','-1001905521512')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = -1001987439557
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002044951526")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://itsme:itsme@cluster0.b4hh3yz.mongodb.net/?retryWrites=true&w=majority")
