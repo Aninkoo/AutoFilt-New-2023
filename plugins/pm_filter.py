09f70c219b3fbe31ada2c3272beb012478154f6a
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-12-28 01:27:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1872,16 +1872,7 @@ async def advantage_spell_chok(client, msg):
     user = msg.from_user.id if msg.from_user else 0
     movielist = []
     gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates https://stackoverflow.com/a/7961425
-'''    if len(gs_parsed) > 3:
-        gs_parsed = gs_parsed[:3]
-    if gs_parsed:
-        for mov in gs_parsed:
-            imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
-            if imdb_s:
-                for movie in imdb_s:
-                    steve=movie.get('title')
-                    if len(steve)<10:
-                        movielist += [(re.sub(r'(\-|\(|\)|_|&|\")', '', steve, flags=re.IGNORECASE)).strip() ]'''
+
     for i in gs_parsed:
         if len(i) < 10:
             movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() ]
