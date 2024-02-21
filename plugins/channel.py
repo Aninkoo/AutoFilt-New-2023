7588e64d9841df099bc3c3d9f210a8d67d9aa71f
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-02-21 15:46:23 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,9 +1,9 @@
-from pyrogram import Client, filters
-from info import CHANNELS
+from pyrogram import Client, filters, enums
+from info import CHANNELS, UPDATES_CHNL
 from database.ia_filterdb import save_file
+from utils import add_chnl_message
 
 media_filter = filters.document | filters.video
-
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
@@ -17,4 +17,9 @@ async def media(bot, message):
 
     media.file_type = file_type
     media.caption = message.caption
-    await save_file(media)
+    text = await save_file(media)
+    if text is not None:
+        final = await add_chnl_message(text)
+        if final is not None:
+            cap = "#MovieUpdate:\n\n"
+            await bot.send_message(chat_id=UPDATES_CHNL, text=f"<b>{cap}<code>{final}</code>\n\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>", disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
