48330ccd9eaf642dca626d8ecb53edde7b38718d
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-10-02 00:54:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -143,6 +143,7 @@ async def get_poster(query, bulk=False, id=False, file=None):
 async def broadcast_messages(user_id, message):
     try:
         await message.copy(chat_id=user_id)
+        await message.pin()
         return True, "Success"
     except FloodWait as e:
         await asyncio.sleep(e.x)
