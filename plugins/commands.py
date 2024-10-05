97841898a79b430085b00cf4f2cf2ad5cfa75971
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-10-06 01:04:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -103,9 +103,11 @@ async def start(client, message):
 
         if message.command[1] != "subscribe" or message.command[1] != "send_all":
             try:
+                kk, file_id = message.command[1].split("_", 1)
+                pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton("🔄 𝖳𝗋𝗒 𝖠𝗀𝖺𝗂𝗇 🔄", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
             except (IndexError, ValueError):
-                pass
+                btn.append([InlineKeyboardButton("🔄 𝖳𝗋𝗒 𝖠𝗀𝖺𝗂𝗇 🔄", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
             await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
