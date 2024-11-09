94f43b51c7564ea4ac22ec3551143a69d1f01116
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-11-10 00:51:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '18037664'))
 API_HASH = environ.get('API_HASH', '2f30344d1a5d5fefc42241ab6c65d02d')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6108815912:AAHWjkYZqeABg8Vo7Negy4H_srfAXo5iUwE')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5221907668:AAHY7SJ7TER85n7ycLfOQbBd0F_PIkfsXSI')
 PORT = environ.get("PORT", "8080")
 
 # Bot settings
