04816c7624aa0a36b1bc42064dd2deb5631aea5d
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-12-30 23:22:22 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import INDEX_CHANNELS, INDEX_EXTENSIONS
+from info import CHANNELS, INDEX_EXTENSIONS
 from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video
