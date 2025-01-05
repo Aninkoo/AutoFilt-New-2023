0622bbf8bf1f229ed88e8906b657fc5b9f25b8e8
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2025-01-05 19:03:50 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -24,7 +24,6 @@ class Media(Document):
     file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
     mime_type = fields.StrField(allow_none=True)
     caption = fields.StrField(allow_none=True)
 
@@ -52,7 +51,6 @@ async def save_file(media):
             file_ref=file_ref,
             file_name=file_name,
             file_size=media.file_size,
-            file_type=media.file_type,
             mime_type=media.mime_type,
             caption=media.caption if media.caption else file_name,
         )
