96a614e1bc31ba1485a31ce98a0781ce0991db47
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-02-26 22:16:48 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -77,6 +77,18 @@ async def getEpisode(filename):
     if match:
             return match.group(1)
 
+async def getSeason(filename):
+    match = re.search(
+        r'''(?ix)                 # Ignore case (i), and use verbose regex (x)
+        (?:                       # non-grouping pattern
+          s|season|^           # e or x or episode or start of a line
+          )                       # end non-grouping pattern 
+        \s*                       # 0-or-more whitespaces
+        (\d{2})                   # exactly 2 digits
+        ''', filename)
+    if match:
+            return match.group(1)
+
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         query = (query.strip()).lower()
