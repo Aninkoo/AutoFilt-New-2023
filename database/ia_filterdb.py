b5a4034180b55cc8bc47e655ea1ac534c83678c6
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-03-03 08:05:51 +0100

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -117,10 +117,12 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     cursor.sort('$natural', -1)
 
     if lang:
-        lang_files =[
+        pattern = rf"\b{re.escape(lang)}(?:\d{{2}})?\b"
+
+        lang_files = [
         file async for file in cursor 
-        if (file.caption and re.search(rf"\b{re.escape(lang)}", file.caption.lower())) 
-        or (file.file_name and re.search(rf"\b{re.escape(lang)}", file.file_name.lower()))
+        if (file.caption and re.search(pattern, file.caption.lower()))
+        or (file.file_name and re.search(pattern, file.file_name.lower()))
         ]
         files = lang_files[offset:][:max_results]
         total_results = len(lang_files)
