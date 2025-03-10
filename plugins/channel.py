980955b689d6114cdfb04c0ab34bdaf937cbe9a0
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-03-10 03:29:31 +0100

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -34,6 +34,7 @@ async def media(bot, message):
     season = await getSeason(mv_naamf)
     episode = await getEpisode(mv_naamf)
     # Fetch the last message from UPDATES_CHNL
+    '''
     last_messages = await bot.get_chat_history(UPDATES_CHNL, limit=1)
     
     last_msg = last_messages[0]
@@ -50,6 +51,7 @@ async def media(bot, message):
         # Delete last message if conditions match
         if last_name == mv_naam and last_episode > (episode or 0):
             await bot.delete_messages(UPDATES_CHNL, last_msg.message_id)
+    '''
     if season == None:
         season = 1
 
