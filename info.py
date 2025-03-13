897c31e627b085f1fa68e702a58d40da92f4d1a0
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-03-13 02:19:44 +0100

    Update info.py

--- info.py
+++ info.py
@@ -38,6 +38,8 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1392566136').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+ASIA_CHNL = int(environ.get('ASIA_CHNL', ''))
+ENG_CHNL = int(environ.get('ENG_CHNL', ''))
 UPDATES_CHNL = int(environ.get('UPDATES_CHNL', ''))
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
