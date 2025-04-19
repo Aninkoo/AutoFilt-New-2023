e4fe8694c535d5d6338da0b5a5bdcf5c2bdfcdfb
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-04-19 09:18:22 +0100

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -21,12 +21,12 @@ instance = Instance.from_db(db)
 @instance.register
 class Media(Document):
     file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True, default=None)
+    file_ref = fields.StrField(allow_none=True, missing=None)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True, default=None)
-    mime_type = fields.StrField(allow_none=True, default=None)
-    caption = fields.StrField(allow_none=True, default=None)
+    file_type = fields.StrField(allow_none=True, missing=None)
+    mime_type = fields.StrField(allow_none=True, missing=None)
+    caption = fields.StrField(allow_none=True, missing=None)
 
     class Meta:
         indexes = ('$file_name', )
